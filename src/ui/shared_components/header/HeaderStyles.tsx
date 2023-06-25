f21d6e365b518e1d751357c632fb797162a08891
Nana Kwame <ynkwame@gmail.com>
2023-06-25 09:53:47 +0000

    Added logo

--- src/ui/shared_components/header/HeaderStyles.tsx
+++ src/ui/shared_components/header/HeaderStyles.tsx
@@ -1,10 +1,14 @@
 import { LogoStyles } from "../logo/LogoStyles";
 import Container from "../atoms/container/Container";
+import logo from "../../../assets/logo_eureka.svg";
 
 const HeaderStyles = () => {
   return (
     <Container h="35px" align="end" justify="end">
-      <LogoStyles />
+      <LogoStyles>
+        {" "}
+        <img width={"100%"} src={logo} alt="Logo" />
+      </LogoStyles>
     </Container>
   );
 };
