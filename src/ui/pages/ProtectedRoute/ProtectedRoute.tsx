7319e19f01761f6ee2c26dd2d3e2319309065a3f
Nana Kwame <ynkwame@gmail.com>
2023-11-07 23:09:05 +0000

    Added user jwt verification

--- src/ui/pages/ProtectedRoute/ProtectedRoute.tsx
+++ src/ui/pages/ProtectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,45 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { AppRoutes } from "../../types/routing";
+import { useEffect, useState } from "react";
+import { adminService } from "../../../services";
+import { IAdmin } from "../../../core";
+import { jwtDecode } from "jwt-decode";
 
 const ProtectedRoute = () => {
-  const loggedInUser = localStorage.getItem("isAdminLocal")
-    ? JSON.parse(localStorage.getItem("isAdminLocal") as string)
-    : null;
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const token = searchParams.get("jwt");
+
+    try {
+      if (token) {
+        const decodedToken = jwtDecode<IAdmin>(token);
+        verifyDecodedToken(decodedToken);
+      } else {
+        console.log("none");
+        <Navigate to={AppRoutes.ROOT} />;
+      }
+    } catch (error) {
+      console.error("JWT decoding error:", error);
+    }
+  }, []);
+
+  async function verifyDecodedToken(decodedToken: IAdmin) {
+    try {
+      const data = await adminService.initAdmin(decodedToken.email);
+
+      if (data) {
+        localStorage.setItem("isAdminLocal", JSON.stringify(data));
+      } else {
+        console.error("Admin data retrieval failed.");
+      }
+    } catch (error) {
+      console.error("Admin data retrieval error:", error);
+    }
+  }
+
+  const isAdminLocal = localStorage.getItem("isAdminLocal");
+  const loggedInUser = isAdminLocal ? JSON.parse(isAdminLocal) : null;
+
   return loggedInUser ? <Outlet /> : <Navigate to={AppRoutes.ROOT} />;
 };
 
